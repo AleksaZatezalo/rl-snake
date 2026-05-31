e820c57693e19c9b106e1b3cbbc01c525ad7c1c1
Aleksa <zabumaphu@gmail.com>
2026-05-31 12:00:31 -0400

    adding automove

--- game.py
+++ game.py
@@ -3,6 +3,7 @@ import pygame
 import random
 from enum import Enum
 from collections import namedtuple
+import numpy as np
 
 pygame.init()
 font = pygame.font.Font('arial.ttf', 25)
@@ -123,6 +124,11 @@ class SnakeGameAI:
         pygame.display.flip()
         
     def _move(self, action):
+        # [straight, right, left]
+
+        clock_wise = [Direction.RIGHT, Direction.DOWN, Direction.LEFT, Direction.up]
+        idx = clock_wise.index(self.direction)
+
         x = self.head.x
         y = self.head.y
         if action == Direction.RIGHT:
